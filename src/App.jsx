d91bb997562bc0804ba676fe4c7f6784353e17dc
Adi <121740959+Adi020@users.noreply.github.com>
2024-07-09 15:22:47 -0600

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -61,7 +61,7 @@ function App() {
         <form className="w-full min-[640px]:pt-4" onSubmit={handleSubmit}>
           <div className="flex p-[2px] overflow-hidden justify-center items-center bg-[#53a8ca] rounded-3xl h-10">
             <button className="w-9 flex justify-center"><i className="bx bx-search text-xl"></i></button>
-            <input className="w-full h-full rounded-full text-xl grid bg-transparent text-white placeholder-white bg-[#03cdff] px-2 outline-none" type="text" placeholder="search" id="cityName" />
+            <input className="w-full h-full rounded-full text-xl grid text-white placeholder-white bg-[#03cdff] px-2 outline-none" type="text" placeholder="search" id="cityName" />
           </div>
 
         </form>
