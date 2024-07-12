7b805ca91149b68a28d92174cdae612230a1766c
Adi <121740959+Adi020@users.noreply.github.com>
2024-07-12 15:06:29 -0600

    Update Header.jsx

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,15 +1,20 @@
 import CitiesList from "./cities/CitiesList"
+import React, { useRef } from 'react';
+
 
 const Header = ({
-  handleSubmit,
   cityName,
   setCityName,
   citiesInfo,
   fetchWeather,
   setIsShowModal,
   isShowModal,
-  loadingCities
+  loadingCities,
+  hiddenModal,
+  fetchCities
 }) => {
+  const formRef = useRef(null);
+
   const handleInputChange = (e) => {
     const inputValue = e.target.value
     setCityName(inputValue)
@@ -19,14 +24,33 @@ const Header = ({
     setIsShowModal(true)
   }
 
-  const hiddenModal = () => {
-    setIsShowModal(false)
-  }
+  // Manejador para el envío del formulario
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    if (formRef.current) {
+      const elements = formRef.current.elements;
+      for (let i = 0; i < elements.length; i++) {
+        elements[i].blur();
+      }
+    }
+    const city = e.target.cityName.value;
+    setCityName(city)
+
+    if (cityName) {
+      const submitCity = () => {
+        const { lat, lon } = citiesInfo[0]
+        console.log(citiesInfo)
+        fetchWeather(lat, lon)
+      }
+      fetchCities(submitCity)
+    }
+    hiddenModal()
+  };
 
   return (
     <header className='z-20 min-[640px]:pt-4 grid grid-rows-[max-content]'>
       <div className="relative">
-        <form onSubmit={handleSubmit}>
+        <form ref={formRef} onSubmit={handleSubmit}>
           <div className="flex p-[2px] justify-center items-center bg-[#53a8ca] rounded-3xl h-10">
             <button className="w-9 flex justify-center"><i className="bx bx-search text-xl"></i></button>
             <input onFocus={showModal} value={cityName}
