60646960bc27aede8660e2182f27a96b3f553de5
Adi <121740959+Adi020@users.noreply.github.com>
2024-07-13 23:39:56 -0600

    Update Header.jsx

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -60,7 +60,7 @@ const Header = ({
 					<div className="flex p-[2px] justify-center items-center bg-[#53a8ca] rounded-3xl h-10">
 						<button className="w-9 flex justify-center"><i className="bx bx-search text-xl"></i></button>
 						<input onFocus={showModal} value={cityName}
-							onChange={handleInputChange} className="w-full h-full rounded-full text-xl grid text-white 
+							onChange={handleInputChange} autocomplete='off' className="w-full h-full rounded-full text-xl grid text-white 
             placeholder-white bg-[#03cdff] px-2 outline-none" type="text" placeholder="search" id="cityName" />
 					</div>
 				</form>
