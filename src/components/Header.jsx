fc247e4a2b3e4f3d4d1637363847d8cab99fa550
Adi <121740959+Adi020@users.noreply.github.com>
2024-07-13 23:25:20 -0600

    Update Header.jsx

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,76 +1,79 @@
+import { axiosWeather } from "../utils/ConfigAxios";
 import CitiesList from "./cities/CitiesList"
 import React, { useRef } from 'react';
 
-
 const Header = ({
-  cityName,
-  setCityName,
-  citiesInfo,
-  fetchWeather,
-  setIsShowModal,
-  isShowModal,
-  loadingCities,
-  hiddenModal,
-  fetchCities
+	cityName,
+	setCityName,
+	citiesInfo,
+	fetchWeather,
+	setIsShowModal,
+	isShowModal,
+	loadingCities,
+	hiddenModal,
+	controller
 }) => {
-  const formRef = useRef(null);
+	const formRef = useRef(null);
 
-  // manjejador del input controlado
-  const handleInputChange = (e) => {
-    const inputValue = e.target.value
-    setCityName(inputValue)
-  }
+	//obtener una ciudad
+	const fetchCity = () => {
+		const URLCity = `geo/1.0/direct?q=${cityName}&limit=1`;
+		axiosWeather.get(URLCity)
+			.then(({ data }) => {
+				const { lat, lon } = data[0]
+				fetchWeather(lat, lon)
+			})
+			.catch(err => console.log(err))
+	}
 
-  // mostrar el modal
-  const showModal = () => {
-    setIsShowModal(true)
-  }
+	// manjejador del input controlado
+	const handleInputChange = (e) => {
+		const inputValue = e.target.value
+		setCityName(inputValue)
+	}
 
-  //funcion para agregar la primera ciudad encontrada
-  const submitCity = (cities) => {
-    const { lat, lon } = cities[0]
-    fetchWeather(lat, lon)
-  }
+	// mostrar el modal
+	const showModal = () => {
+		setIsShowModal(true)
+	}
 
-  // Manejador para el envío del formulario
-  const handleSubmit = (e) => {
-    e.preventDefault();
+	// Manejador para el envío del formulario
+	const handleSubmit = (e) => {
+		e.preventDefault();
+		controller.abort()
+		if (formRef.current) {
+			const elements = formRef.current.elements;
+			for (let i = 0; i < elements.length; i++) {
+				elements[i].blur();
+			}
+		}
+		const city = e.target.cityName.value;
+		setCityName(city)
+		fetchCity()
+		hiddenModal()
+	};
 
-    if (formRef.current) {
-      const elements = formRef.current.elements;
-      for (let i = 0; i < elements.length; i++) {
-        elements[i].blur();
-      }
-    }
-    const city = e.target.cityName.value;
-
-    setCityName(city)
-    fetchCities(submitCity)
-    hiddenModal()
-  };
-
-  return (
-    <header className='z-20 min-[640px]:pt-4 grid grid-rows-[max-content]'>
-      <div className="relative">
-        <form ref={formRef} onSubmit={handleSubmit}>
-          <div className="flex p-[2px] justify-center items-center bg-[#53a8ca] rounded-3xl h-10">
-            <button className="w-9 flex justify-center"><i className="bx bx-search text-xl"></i></button>
-            <input onFocus={showModal} value={cityName}
-              onChange={handleInputChange} className="w-full h-full rounded-full text-xl grid text-white 
+	return (
+		<header className='z-20 min-[640px]:pt-4 grid grid-rows-[max-content]'>
+			<div className="relative">
+				<form ref={formRef} onSubmit={handleSubmit}>
+					<div className="flex p-[2px] justify-center items-center bg-[#53a8ca] rounded-3xl h-10">
+						<button className="w-9 flex justify-center"><i className="bx bx-search text-xl"></i></button>
+						<input onFocus={showModal} value={cityName}
+							onChange={handleInputChange} className="w-full h-full rounded-full text-xl grid text-white 
             placeholder-white bg-[#03cdff] px-2 outline-none" type="text" placeholder="search" id="cityName" />
-          </div>
-        </form>
+					</div>
+				</form>
 
-        <CitiesList
-          citiesInfo={citiesInfo}
-          fetchWeather={fetchWeather}
-          cityName={cityName}
-          isShowModal={isShowModal}
-          hiddenModal={hiddenModal}
-          loadingCities={loadingCities} />
-      </div>
-    </header>
-  )
+				<CitiesList
+					citiesInfo={citiesInfo}
+					fetchWeather={fetchWeather}
+					isShowModal={isShowModal}
+					hiddenModal={hiddenModal}
+					loadingCities={loadingCities} />
+			</div>
+		</header>
+	)
 }
 
 export default Header
